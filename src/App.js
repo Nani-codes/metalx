25f0cabb25f9221d36a1ce18bdf4425dc681b3d0
Nani <lakshminarayanamalineni1@gmail.com>
2024-06-28 16:01:33 +0530

    Test Deploy

--- src/App.js
+++ src/App.js
@@ -16,19 +16,43 @@ import ProductDetails from './pages/ProductDetails';
 import AuctionDetails from './pages/AuctionDetails';
 import ExpertsList from './pages/ExpertsList';
 import ExpertDetails from './pages/ExpertDetails';
-import Footer from './components/Footer';
+import Footer from './landingcomponents/Footer';
 import DashBoard from './components/DashBoard';
 import Cart from './components/Cart';
+import LandingPage from './pages/LandingPage'
+import getLPTheme from './pages/getLPTheme';
+import AppAppBar from './landingcomponents/AppAppBar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Divider from '@mui/material/Divider';
+
+
 // import TableFooter from '@mui/material/TableFooter';
 
 
 function App() {
+  const [mode, setMode] = React.useState('light');
+  const [showCustomTheme, setShowCustomTheme] = React.useState(true);
+  const LPtheme = createTheme(getLPTheme(mode));
+  const defaultTheme = createTheme({ palette: { mode } });
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === 'dark'? 'light' : 'dark'));
+  };
+
+  const toggleCustomTheme = () => {
+    setShowCustomTheme((prev) =>!prev);
+  };
+
+
   return (
+    
+    <ThemeProvider theme={showCustomTheme? LPtheme : defaultTheme}>
     <Router>
-      {/* <Header /> */}
-      <Navbar />
+      {/* <Navbar /> */}
+      <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
+
       <Routes>
-        <Route path="/" element={<Home />} />        
+        <Route path="/" element={<LandingPage />} />        
         <Route path="/Home" element={<Home />} />        
         <Route path="/Sell" element={<Sell />} />        
         <Route path="/login" element={<Login />} />
@@ -46,8 +70,10 @@ function App() {
         <Route path="/dashboard" element={<DashBoard />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
-      <Footer/>
     </Router>
+    <Divider/>
+    <Footer/>
+    </ThemeProvider>
   );
 }
 
